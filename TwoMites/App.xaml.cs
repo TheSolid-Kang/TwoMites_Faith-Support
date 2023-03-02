c2ce7c2f2f07f17099be25af91d7d66f315ab5e4
TheSolid-KANG <tortoise8947@gmail.com>
2023-03-02 19:57:34 +0900

    STWOM-16 프로세스 런 가드 적용

--- TwoMites/App.xaml.cs
+++ TwoMites/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using TwoMites_Engine._00.CEngine_TwoMites;
@@ -19,11 +20,30 @@ namespace TwoMites
             System.Resources.ResourceManager resources = new System.Resources.ResourceManager(typeof(App));
             initialize();
         }
+        public CEngine_TwoMites m_engineTwoMites { get; set; }
+        private Mutex mutex;
         private void initialize()
         {
             m_engineTwoMites = new CEngine_TwoMites();
         }
-        public CEngine_TwoMites m_engineTwoMites { get; set; }
+        //프로세스 런 가드
+        //https://afsdzvcx123.tistory.com/entry/WPF-WPF-%EC%A4%91%EB%B3%B5-%EC%8B%A4%ED%96%89-%EB%B0%A9%EC%A7%80-%ED%95%98%EB%8A%94%EB%B2%95
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            string mutexName = "program";
+            bool createNew = false;
+
+            mutex = new Mutex(true, mutexName, out createNew);
+
+            if( false == createNew )
+            {
+                System.Windows.Forms.MessageBox.Show("이미 실행중입니다.");
+                Shutdown();
+            }
+
+        }
     }
 
 }
